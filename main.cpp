84183a0bde782cfc93867949348e5bb2e560e462
Sina Aghli <Sina@engr2-15-210-dhcp.int.colorado.edu>
2015-03-22 19:29:26 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "GetPot"
 
 FtdiDriver  g_FtdiDriver;
-//node::node  nc_node;
+node::node  nc_node;
 
 NinjaStateMsg BuildNinjaStateMsg( const SensorPacket& p )
 {
@@ -35,60 +35,56 @@ NinjaStateMsg BuildNinjaStateMsg( const SensorPacket& p )
   msg.set_adc_rf( p.ADC_RF );
 
   return msg;
-};
+}
 
 int main( int argc, char** argv )
 {
   GetPot cl(argc,argv);
   std::string dev = cl.follow("/dev/ttyUSB0","--dev");
 
-  //nc_node.init("nc_node");
+  nc_node.init("nc_node");
 
   printf("Connecting to FTDI com port '%s'...\n", dev.c_str() );
   g_FtdiDriver.Connect(dev);
   // subscribe to the ninja_command topic
-  //nc_node.subscribe("ninja_commander/command");
+  nc_node.subscribe("ninja_commander/command");
 
   // advertise that we will transmit state data
-  //if( nc_node.advertise("state") == false ) {
-  //  printf("Error setting publisher.\n");
-  //}
+  if( nc_node.advertise("state") == false ) {
+    printf("Error setting publisher.\n");
+  }
 
 
   for( size_t ii = 0; ; ++ii ) {
-//    if( ii % 1000 == 0 ) {
-//      printf(".");
-//      fflush(stdout);
-//    }
 
     // read from the car's microcontroller
     SensorPacket state;
-    if( g_FtdiDriver.ReadSensorPacket(state) == 0 ) {
-      printf("Ftdi read returned 0\n");
-    }else
-    {
-      printf("*********************************************************");
-      printf("acc_x: %d  acc_y: %d  acc_z: %d \n",state.Acc_x,state.Acc_y,state.Acc_z);
-      printf("Gyro_x: %d  Gyro_y: %d  Gyro_z: %d \n",state.Gyro_x,state.Gyro_y,state.Gyro_z);
-      printf("Mag_x: %d  Mag_y: %d  Mag_z: %d \n",state.Mag_x,state.Mag_y,state.Mag_z);
-      printf("Enc_lb: %d  Enc_lf: %d  Enc_rb: %d  Enc_rf: %d \n",state.Enc_LB,state.Enc_LF,state.Enc_RB,state.Enc_RF);
-      printf("adc_steer: %d  adc_lb: %d  adc_lf: %d  adc_rb: %d  adc_rf: %d \n", state.ADC_Steer,state.ADC_LB,state.ADC_LF,state.ADC_RB,state.ADC_RF);
+//    if( g_FtdiDriver.ReadSensorPacket(state) == 0 ) {
+//      printf("Ftdi read returned 0\n");
+//    }else
+//    {
+//      printf("*********************************************************\n");
+//      printf("acc_x: %d  acc_y: %d  acc_z: %d \n",state.Acc_x,state.Acc_y,state.Acc_z);
+//      printf("Gyro_x: %d  Gyro_y: %d  Gyro_z: %d \n",state.Gyro_x,state.Gyro_y,state.Gyro_z);
+//      printf("Mag_x: %d  Mag_y: %d  Mag_z: %d \n",state.Mag_x,state.Mag_y,state.Mag_z);
+//      printf("Enc_lb: %d  Enc_lf: %d  Enc_rb: %d  Enc_rf: %d \n",state.Enc_LB,state.Enc_LF,state.Enc_RB,state.Enc_RF);
+//      printf("adc_steer: %d  adc_lb: %d  adc_lf: %d  adc_rb: %d  adc_rf: %d \n", state.ADC_Steer,state.ADC_LB,state.ADC_LF,state.ADC_RB,state.ADC_RF);
     }
 
     // package and send our state protobuf
     NinjaStateMsg state_msg = BuildNinjaStateMsg( state );
-//    if ( nc_node.publish( "state", state_msg ) == false ) {
-//      printf("Error sending message.\n");
-//    }
+    if ( nc_node.publish( "state", state_msg ) == false ) {
+      printf("Error sending message.\n");
+    }
 
     // let's see what the controller is telling us to do
     NinjaCommandMsg cmd;
-//    if( nc_node.receive("ninja_commander/command", cmd) ){
-//      printf("Received a=%.2f and p=%.2f\n", cmd.speed(), cmd.turnrate() );
-      // relay command to the car 
-      //g_FtdiDriver.SendCommandPacket( cmd.speed(), cmd.turnrate() );
-      g_FtdiDriver.SendCommandPacket( 123, 45 );
-//    }
+    if( nc_node.receive("ninja_commander/command", cmd) ){
+      printf("Received a=%.2f and p=%.2f\n", cmd.speed(), cmd.turnrate() );
+      // relay command to the car
+      g_FtdiDriver.SendCommandPacket( cmd.speed(), cmd.turnrate() );
+      //g_FtdiDriver.SendCommandPacket( 123, 45 );
+    }
     usleep(100000);
   }
 
